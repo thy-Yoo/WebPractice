4248dbde12b703eafe9471eb6db8b3696207537e
LitlOuo <86100707+thy-Yoo@users.noreply.github.com>
2021-09-17 09:45:19 +0900

    Update ViploungeDAO.java

--- MovieWeb06/src/com/sist/dao/ViploungeDAO.java
+++ MovieWeb06/src/com/sist/dao/ViploungeDAO.java
@@ -14,7 +14,7 @@ public class ViploungeDAO {
 			// SQL문장 전송 객체
 			private PreparedStatement ps;
 			// 오라클 서버 주소
-			private final String URL = "jdbc:oracle:thin:@litlyoo:1521:XE";
+			private final String URL = "jdbc:oracle:thin:@오라클주소:1521:XE";
 
 			// 1. 드라이버 등록
 			public ViploungeDAO() {
@@ -27,7 +27,7 @@ public class ViploungeDAO {
 			// 2. 오라클 연결
 			public void getConnection() {
 				try {
-					conn = DriverManager.getConnection(URL, "ouo", "litl");
+					conn = DriverManager.getConnection(URL, "오라클id", "오라클pw");
 					// conn hr/happy
 				} catch (Exception ex) {
 				}
